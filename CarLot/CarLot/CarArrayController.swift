41769a38a4b657d49c13687481d56aea6803f179
jwheeler79 <jwheeler1@gmail.com>
2015-11-07 13:09:24 -0800

    xtra credit

--- CarLot/CarLot/CarArrayController.swift
+++ CarLot/CarLot/CarArrayController.swift
@@ -10,10 +10,24 @@ import Cocoa
 
 class CarArrayController: NSArrayController {
 
+    @IBOutlet weak var tableView: NSTableView!
+    
     override func newObject() -> AnyObject {
         let newObj = super.newObject() as! NSObject
         let now = NSDate()
         newObj.setValue(now, forKey: "datePurchased")
         return newObj
+    }
+    
+    // MARK: - Actions
+    
+    // X-tra credit :-)
+    @IBAction func addCar(sender: NSButton) {
+        let car = newObject() as! NSObject
+        addObject(car)
+        rearrangeObjects()
+        let sortedCars = arrangedObjects as! [NSObject]
+        let row = sortedCars.indexOf(car)!
+        tableView.editColumn(0, row: row, withEvent: nil, select: true)
     }
 }
