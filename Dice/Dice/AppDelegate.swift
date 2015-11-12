636cb5976a97434c9382c84b93ce38cbef1e21a5
jwheeler79 <jwheeler1@gmail.com>
2015-11-12 08:14:12 -0800

    completed 'Dice' exercise on NSView

---
+++ Dice/Dice/AppDelegate.swift
@@ -0,0 +1,25 @@
+//
+//  AppDelegate.swift
+//  Dice
+//
+//  Created by John Wheeler on 11/11/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    var mainWindowController: MainWindowController?
+    
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        let mainWindowController = MainWindowController()
+        mainWindowController.showWindow(self)
+        self.mainWindowController = mainWindowController
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+    }
+}
+
---
+++ Dice/Dice/MainWindowController.swift
@@ -0,0 +1,20 @@
+//
+//  MainWindowController.swift
+//  Dice
+//
+//  Created by John Wheeler on 11/11/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import Cocoa
+
+class MainWindowController: NSWindowController {
+    
+    override var windowNibName: String {
+        return "MainWindowController"
+    }
+    
+    override func windowDidLoad() {
+        super.windowDidLoad()
+    }
+}
