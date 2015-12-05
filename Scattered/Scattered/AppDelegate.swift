7da25e6ec1a5727ec7d3202ab33901f3b515eef6
jwheeler79 <jwheeler1@gmail.com>
2015-12-05 10:15:19 -0800

    Chapter 33 - Core Animation - Scattered

---
+++ Scattered/Scattered/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Scattered
+//
+//  Created by John Wheeler on 12/5/15.
+//  Copyright © 2015 Big Nerd Ranch. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
